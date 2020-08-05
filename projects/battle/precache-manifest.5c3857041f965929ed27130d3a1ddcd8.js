c8f04eb16d2af29f0e73c2a1dd07ca95bf6a5367
Humberto Suastegui <hsuastegui@gmail.com>
2020-08-05 08:35:53 +0100

    add project

---
+++ projects/battle/precache-manifest.5c3857041f965929ed27130d3a1ddcd8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "693935c8d28a3931329813da947b173c",
+    "url": "/projects/battle/index.html"
+  },
+  {
+    "revision": "e6e803ffbd58f29f2002",
+    "url": "/projects/battle/static/css/main.91c1e2e4.chunk.css"
+  },
+  {
+    "revision": "42bf8aebc3193c688b1b",
+    "url": "/projects/battle/static/js/2.d5b662ac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/projects/battle/static/js/2.d5b662ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6e803ffbd58f29f2002",
+    "url": "/projects/battle/static/js/main.65f93229.chunk.js"
+  },
+  {
+    "revision": "54eb4b9c31fa1b2eab82",
+    "url": "/projects/battle/static/js/runtime-main.8ff14749.js"
+  },
+  {
+    "revision": "ca114c67e2f8e66f8f3c26c797c7e893",
+    "url": "/projects/battle/static/media/monster.ca114c67.png"
+  },
+  {
+    "revision": "38993b84b413402a75477064112eac44",
+    "url": "/projects/battle/static/media/player.38993b84.png"
+  }
+]);
